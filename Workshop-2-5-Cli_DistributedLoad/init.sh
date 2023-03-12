a23f5a350ca718f1a93948b4ecddb3230bdc87e7
Praparn Lungpoonlap <eva10409@gmail.com>
2023-03-12 22:40:18 +0700

    20230312224016

--- Workshop-2-5-Cli_DistributedLoad/init.sh
+++ Workshop-2-5-Cli_DistributedLoad/init.sh
@@ -3,8 +3,8 @@ echo "Step 1: Define IP Address, Timestamp Volume of JMeter Server and JMeter Cl
 CLIENT_IP=192.168.100.20
 declare -a SERVER_IPS=("192.168.100.10" "192.168.100.11" "192.168.100.12")
 timestamp=$(date +%Y%m%d_%H%M%S)
-script_path=~/jmeter-workshop-072021/Workshop-2-5-Cli_DistributedLoad/jmeter
-result_path=~/jmeter-workshop-072021/Workshop-2-5-Cli_DistributedLoad/result
+script_path=~/jmeter-workshop-032023/Workshop-2-5-Cli_DistributedLoad/jmeter
+result_path=~/jmeter-workshop-032023/Workshop-2-5-Cli_DistributedLoad/result
 jmeter_scriptpath=/mnt/jmeter
 jmeter_resultpath=/mnt/result
 
@@ -14,7 +14,7 @@ do
 	docker run --name $IPADDR \
 	-dit --net jmeternet --ip $IPADDR \
   --mount type=bind,source=${result_path},target=${jmeter_resultpath} \
-	--rm labdocker/jmeter:lab -n -s \
+	--rm labdocker/jmeter:lab032023 -n -s \
 	-Jclient.rmi.localport=7000 -Jserver.rmi.localport=60000 -Jserver.rmi.ssl.disable=true \
 	-j ${jmeter_resultpath}/server/slave_${timestamp}_${IPADDR}.log 
 done
@@ -25,7 +25,7 @@ docker run --name $CLIENT_IP \
   --mount type=bind,source=${result_path},target=${jmeter_resultpath} \
   --mount type=bind,source=${script_path},target=${jmeter_scriptpath} \
   -v "${volume_path}":${jmeter_path} \
-  --rm labdocker/jmeter:lab -n -X \
+  --rm labdocker/jmeter:lab032023 -n -X \
   -Jclient.rmi.localport=7000 -Jserver.rmi.ssl.disable=true \
   -R $(echo $(printf ",%s" "${SERVER_IPS[@]}") | cut -c 2-) \
   -t ${jmeter_scriptpath}/Template_Workshop_2_5_FirstPage.jmx \
